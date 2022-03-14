bb1976a41c634d7d4f1f873fadedf43336d1be72
MudassirAhmed0 <mudassir0ahmed1@gmail.com>
2022-03-14 11:07:31 +0500

    origin

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,8 @@
- import Head from  'next/head'
-
+  
 export default function Home() {
   return (
-    <>
-      <Head>
-        <title>Home || Zajil</title>
-        <meta name="description" content="" />
-        
-      </Head>
-       <>
+   
       <h1>Chutiya</h1>
-      </>
-    
-    </>
+     
   )
 }
